ec103d18d2dff3812b4b5fb609cc5c408980462e
Vladyslav <vladikzizu@gmail.com>
2020-07-11 12:10:44 +0200

    add mask phone

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
 import modals from './modules/modals';
 import sliders from './modules/sliders';
+import forms from './modules/forms';
+import mask from './modules/mask';
 
 
 window.addEventListener('DOMContentLoaded', () =>{
@@ -8,4 +10,6 @@ window.addEventListener('DOMContentLoaded', () =>{
     modals();
     sliders('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
     sliders('.main-slider-item', 'vertical');
+    forms();
+    mask('[name = "phone"]');
 });
